eddc1b4b031a9e0023f4479f37a6607cf9455cb4
andreyoganesyan <andreyoganesyan.stuff@gmail.com>
2016-09-22 15:43:25 +0300

    Done.

--- CG1/CG1/MainWindow.xaml.cs
+++ CG1/CG1/MainWindow.xaml.cs
@@ -20,9 +20,66 @@ namespace CG1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Sun sun;
+        int angle = 0;
         public MainWindow()
         {
             InitializeComponent();
+            Draw();
+
+
+        }
+
+        void Draw()
+        {
+            canvas.Children.Clear();
+
+            sun = new Sun();
+            canvas.Children.Add(sun);
+
+            Cloud cloud = new Cloud();
+            canvas.Children.Add(cloud);
+            Canvas.SetRight(cloud, 60);
+            Canvas.SetTop(cloud, -100);
+
+            TextBlock tb = new TextBlock();
+            tb.Text = "Я не умею рисовать :(";
+            tb.FontSize = 30;
+            tb.Foreground = Brushes.DeepSkyBlue;
+            canvas.Children.Add(tb);
+            tb.LayoutTransform = new RotateTransform(15);
+
+            Ellipse polyana = new Ellipse();
+            polyana.Fill = new SolidColorBrush(Color.FromRgb(100, 230, 100));
+            polyana.Height = 206;
+            polyana.Width = 898;
+            canvas.Children.Add(polyana);
+            Canvas.SetLeft(polyana, -10);
+            Canvas.SetBottom(polyana, -100);
+            this.MaxWidth = 700;
+
+            Tree tree1 = new Tree();
+            canvas.Children.Add(tree1);
+            Canvas.SetBottom(tree1, 0);
+            Canvas.SetLeft(tree1, -20);
+
+            Tree tree2 = new Tree();
+            canvas.Children.Add(tree2);
+            Canvas.SetBottom(tree2, 0);
+            Canvas.SetRight(tree2, 50);
+
+
+
+
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                Draw();
+                sun.LayoutTransform = new RotateTransform(angle += 15);
+                
+            }
         }
     }
 }
---
+++ CG1/CG1/Tree.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CG1
+{
+    /// <summary>
+    /// Interaction logic for Tree.xaml
+    /// </summary>
+    public partial class Tree : UserControl
+    {
+        static Random rand = new Random();
+        public Tree()
+        {
+            InitializeComponent();
+            Rectangle trunk = new Rectangle();
+
+            trunk.Width = 30 - rand.Next(11);
+            trunk.Height = 35 - rand.Next(11);
+            trunk.Fill = Brushes.Brown;
+            canvas.Children.Add(trunk);
+            Canvas.SetBottom(trunk, 0);
+
+            int numOfTriangles = 3 + rand.Next(3);
+            Polygon[] triangles = new Polygon[numOfTriangles];
+            int level = (int)trunk.Height - 5;
+            int maxwidth = 0;
+            for (int i = 0; i < numOfTriangles; i++)
+            {
+                int width = 75 + 8 * (numOfTriangles) - 17 * (i + 1);
+                maxwidth = maxwidth > width ? maxwidth : width;
+                int height = 45 - 6 * (i + 1);
+
+                Point baseL = new Point(0, 0);
+                Point baseR = new Point(width, 0);
+                Point heightPoint = new Point(width / 2, -height);
+
+                triangles[i] = new Polygon();
+                triangles[i].Points.Add(baseL);
+                triangles[i].Points.Add(baseR);
+                triangles[i].Points.Add(heightPoint);
+                triangles[i].Fill = Brushes.Green;
+                canvas.Children.Add(triangles[i]);
+                Canvas.SetBottom(triangles[i], level);
+                level += height - 8;
+
+            }
+            Canvas.SetLeft(trunk, -trunk.Width / 2 + maxwidth / 2);
+            foreach (Polygon triangle in triangles)
+            {
+                Canvas.SetLeft(triangle, -triangle.Points[1].X / 2 + maxwidth / 2);
+            }
+
+        }
+    }
+}
